8ac74afd78bf3e2226508bf612ee210e69b7a100
Lokusok <lokusokdrobk@gmail.com>
2024-05-12 19:01:23 +0300

    step 10

---
+++ middlewares/cors.js
@@ -0,0 +1,13 @@
+const { CORS } = require('../config');
+
+function cors(req, res, next) {
+  const { origin } = req.headers;
+
+  if (CORS.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+
+  next();
+}
+
+module.exports = { cors };
